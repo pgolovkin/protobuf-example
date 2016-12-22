566f161a00baa21feea0779ca5509a4a87dcb16d
pgolovkin <createme4you>
2016-12-22 20:20:43 +0300

    Add protobuf dependency

--- src/main/java/ru/pgolovkin/protobufexample/application/Application.java
+++ src/main/java/ru/pgolovkin/protobufexample/application/Application.java
@@ -3,11 +3,9 @@ package ru.pgolovkin.protobufexample.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
 
-@SpringBootApplication
-@ComponentScan(basePackages = "ru.pgolovkin.protobufexample")
+@SpringBootApplication(scanBasePackages = "ru.pgolovkin.protobufexample")
 public class Application {
 
     public static void main(String[] args) {
