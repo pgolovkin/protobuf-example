1cbe143354cb6f3a32a9a2be61b338821a25b9c2
pgolovkin <createme4you>
2016-12-22 21:37:06 +0300

    Test protobuf

--- src/main/java/ru/pgolovkin/protobufexample/controller/PersonController.java
+++ src/main/java/ru/pgolovkin/protobufexample/controller/PersonController.java
@@ -2,12 +2,14 @@ package ru.pgolovkin.protobufexample.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.pgolovkin.protobufexample.proto.PersonPRO;
 
 @RestController
 public class PersonController {
 
     @RequestMapping("/")
-    public String index() {
-        return "Hello World!";
+    public PersonPRO.Person index() {
+        PersonPRO.Person personPRO = PersonPRO.Person.newBuilder().setCourseName("1").setId(1).build();
+        return personPRO;
     }
 }
