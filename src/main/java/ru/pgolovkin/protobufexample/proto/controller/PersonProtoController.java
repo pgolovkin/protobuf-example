664309251a41fc7a522e47f387c95664cb1c4a42
pgolovkin <createme4you>
2016-12-27 19:07:00 +0300

    Made proto controllers return binary response

--- src/main/java/ru/pgolovkin/protobufexample/proto/controller/PersonProtoController.java
+++ src/main/java/ru/pgolovkin/protobufexample/proto/controller/PersonProtoController.java
@@ -1,8 +1,13 @@
 package ru.pgolovkin.protobufexample.proto.controller;
 
+import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.pgolovkin.protobufexample.proto.AddressBookPRO.Person;
 import ru.pgolovkin.protobufexample.settings.UrlConstants;
@@ -10,11 +15,18 @@ import ru.pgolovkin.protobufexample.settings.UrlConstants;
 @RestController
 public class PersonProtoController {
 
-    @RequestMapping(path = UrlConstants.PERSON_PROTO_URL, produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
-    public Person getPerson() {
+    @RequestMapping(path = UrlConstants.PERSON_PROTO_URL, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE, method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public void getPerson(HttpServletResponse response) {
+
         Person john = Person.newBuilder().setId(123).setName("name")
             .addPhone(Person.PhoneNumber.newBuilder().setNumber("23213213").setType(Person.PhoneType.HOME)).build();
 
-        return john;
+        try {
+            response.setContentType(MediaType.APPLICATION_OCTET_STREAM.getType());
+            john.writeTo(response.getOutputStream());
+        } catch(IOException ex) {
+            System.out.println(ex);
+        }
     }
 }
